c981b9787a128e3859021d3a8529693ca0b993a9
Georgiy Chorny <georgiy.black@gmail.com>
2020-05-21 21:54:37 +0300

    add commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Title(props) {
-  console.log(props);
-  return (
-    <div>
-      <p>Are you strong</p>
-      <p>{props.title}</p>
-      <p>{props.hey}</p>
-    </div>
-  );
+// function Title(props) {
+//   console.log(props);
+//   return (
+//     <div>
+//       <p>Are you strong</p>
+//       <p>{props.title}</p>
+//       <p>{props.hey}</p>
+//     </div>
+//   );
+// }
+
+// function Main() {
+//   return (
+//     <div>
+//       <p> Are you good </p>
+//       <Title title="are you beautiful" hey="stay at home" />
+//     </div>
+//   );
+// }
+
+const movie = {
+  title: "Moonlight",
+  vote: 9.2,
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/71rNJQ2g-EL._AC_SL1178_.jpg",
+  overwiev: "Loremipsumlalala "
+};
+
+function Image(props) {
+  // console.log ("Image props", props);
+  return <img src={props.src} alt={props.alt} />;
 }
 
-function Main() {
-  return (
-    <div>
-      <p> Are you good </p>
-      <Title title="are you beautiful" hey="stay at home" />
-    </div>
-  );
+// function MovieItem(props) {
+//   console.log("MovieItem props", props);
+//   const { data: { title,vote,image} }= props;
+//   return (
+//     <div>
+//       <Image src={image} alt={title}/>
+//       <p>{title}</p>
+//       <p>{vote}</p>
+//     </div>
+//   );
+// }
+
+class MovieItem extends React.Component {
+  render() {
+    const {
+      data: { title, vote, image }
+    } = this.props;
+    console.log(this);
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote}</p>
+      </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div>
       Hello world!
-      <Main />
+      <MovieItem data={movie} />
     </div>
   );
 }
